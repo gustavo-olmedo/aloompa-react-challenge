93056c38a7327f8f9ccfa1e4655373a1d65b6296
Gustavo Adolfo Olmedo <gustavo.olmedo.formosa@gmail.com>
2025-05-26 00:09:37 +0100

    update config apollo to use real graphql api based on REACT_APP_STAGE in .env file

--- src/config/apollo/index.ts
+++ src/config/apollo/index.ts
@@ -3,12 +3,23 @@ import {
   InMemoryCache,
   from,
   NormalizedCacheObject,
+  ApolloLink,
+  HttpLink,
 } from "@apollo/client";
 
 import { mockLink } from "../../mocks";
 
 const createApolloClient = (): ApolloClient<NormalizedCacheObject> => {
-  const httpLink = mockLink;
+  const REACT_APP_GRAPHQL_API = process.env.REACT_APP_GRAPHQL_API;
+
+  const httpLink =
+    process.env.REACT_APP_STAGE === "development" ||
+    process.env.REACT_APP_STAGE === "test"
+      ? mockLink
+      : ApolloLink.split(
+          (operation) => operation.getContext().public !== true,
+          new HttpLink({ uri: REACT_APP_GRAPHQL_API })
+        );
 
   return new ApolloClient({
     link: from([httpLink]),
