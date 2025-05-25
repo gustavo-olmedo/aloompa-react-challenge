93ccb700591225f4df4e5adde0d3d60f1cb1b727
Gustavo Adolfo Olmedo <gustavo.olmedo.formosa@gmail.com>
2025-05-25 23:12:18 +0100

    add list of event to mock the api response

---
+++ src/mocks/events.ts
@@ -0,0 +1,66 @@
+export const mockEvents = [
+  {
+    id: "1",
+    name: "Live Concert Night",
+    image: "https://images.pexels.com/photos/167636/pexels-photo-167636.jpeg",
+    description:
+      "Experience the thrill of live music with electrifying performances.",
+  },
+  {
+    id: "2",
+    name: "Corporate Seminar",
+    image: "https://images.unsplash.com/photo-1551836022-d5d88e9218df",
+    description:
+      "Join industry leaders for insightful discussions and networking.",
+  },
+  {
+    id: "3",
+    name: "Wedding Celebration",
+    image: "https://images.unsplash.com/photo-1504754524776-8f4f37790ca0",
+    description: "Celebrate love and unity in a beautiful wedding ceremony.",
+  },
+  {
+    id: "4",
+    name: "Art Exhibition",
+    image: "https://images.pexels.com/photos/356079/pexels-photo-356079.jpeg",
+    description:
+      "Explore stunning artworks from emerging and established artists.",
+  },
+  {
+    id: "5",
+    name: "Food Festival",
+    image: "https://images.unsplash.com/photo-1504754524776-8f4f37790ca0",
+    description: "Savor delicious cuisines from around the world in one place.",
+  },
+  {
+    id: "6",
+    name: "Tech Conference",
+    image: "https://images.unsplash.com/photo-1504754524776-8f4f37790ca0",
+    description: "Discover the latest innovations and trends in technology.",
+  },
+  {
+    id: "7",
+    name: "Charity Gala",
+    image: "https://images.pexels.com/photos/3184396/pexels-photo-3184396.jpeg",
+    description: "Support a noble cause while enjoying an elegant evening.",
+  },
+  {
+    id: "8",
+    name: "Marathon Run",
+    image: "https://images.unsplash.com/photo-1508609349937-5ec4ae374ebf",
+    description: "Challenge yourself in a community-driven marathon event.",
+  },
+  {
+    id: "9",
+    name: "Book Fair",
+    image: "https://images.unsplash.com/photo-1504754524776-8f4f37790ca0",
+    description:
+      "Dive into a world of literature and meet your favorite authors.",
+  },
+  {
+    id: "10",
+    name: "Photography Workshop",
+    image: "https://images.pexels.com/photos/1051077/pexels-photo-1051077.jpeg",
+    description: "Enhance your photography skills with hands-on sessions.",
+  },
+];
